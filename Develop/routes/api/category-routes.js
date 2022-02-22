439ed3c40f33bb96ae5eedee2b49b44247b1b0c3
Caitlyn Griffing <caitlyn.griffing@gmail.com>
2022-02-21 18:44:23 -0600

    models completed

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try{
+    const categoryAll = await Category.findAll({
+      include: [{model: Product}]
+    });
+    console.log(categoryAll);
+    res.json(categoryAll);
+  } catch (err) {
+    console.log(err);
+    res.json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+  try {
+    const categorySelect = 
+  }
+});
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
